e0bca52c3c6cc871e98274402285ee56b1206eb4
Felipe <felipe.escorel@gmail.com>
2019-09-26 21:31:16 -0300

    Styles :art:

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,49 +6,101 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   TextInput,
+  SafeAreaView,
+  ScrollView,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  StatusBar
 } from 'react-native';
 
 
 const Login = () => {
   return (
-    <View style={styles.mainView}>
-      <View style={styles.titleLogoBox}>
-        <Text>FeedbackDC</Text>
-      </View>
-      <View style={styles.loginBox}>
-        <Text>LOGIN</Text>
-        <TextInput style={styles.textInput} placeholder="Usuário">
-        </TextInput>
-        <Text>Senha</Text>
-        <TextInput style={styles.textInput} placeholder="Senha">
-        </TextInput>
-      </View>
-      <View style={styles.footerBox}>
-        <Text>Foter1</Text>
-        <Text>Foter2</Text>
-        <Text>Foter3</Text>
-        <Text>Foter4</Text>
-        <Text>Foter5</Text>
-      </View>
-    </View>
+    <Fragment>
+
+      <StatusBar backgroundColor='#f09' />
+      <SafeAreaView>
+        <ScrollView>
+          <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior='position'
+            enabled>
+            <View style={styles.header}>
+              <Text style={styles.title}>
+                FeedbackDC
+              </Text>
+            </View>
+            <View style={styles.viewInput}>
+              <Text>LOGIN</Text>
+              <TextInput style={styles.textInput} placeholder="Usuário">
+              </TextInput>
+              <Text>Senha</Text>
+              <TextInput style={styles.textInput} keyboardType={'numeric'} placeholder="Senha">
+              </TextInput>
+            </View>
+            <View style={styles.containerButton}>
+              <TouchableOpacity style={styles.button}>
+                <Text>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
   );
 };
 
-const styles = StyleSheet.create({
 
-  textInput: {
-    backgroundColor: '#a9a9a9',
+const styles = StyleSheet.create({
+  title: {
+    padding: 20,
+    fontSize: 24,
+    color: '#ffffff',
+    fontWeight: "bold"
   },
-  footerBox: {
+  header: {
+    backgroundColor: '#f09',
     alignItems: "center"
+  },
+  textInput: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 8,
+    fontSize: 16,
+    color: '#008b8b',
+    padding: 10,
+  },
+  viewInput: {
+    paddingHorizontal: 20
+  },
+  gasText: {
+    paddingVertical: 20,
+    fontSize: 20
+  },
+  containerButton: {
+    paddingVertical: 50,
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#008b8b',
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  textButton: {
+    textAlign: 'center',
+    fontSize: 16,
+    alignSelf: "center",
+    color: '#ffffff'
   }
-  
 });
 
 export default Login;
