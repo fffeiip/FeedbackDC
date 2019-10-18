642006c59e4f1b761e2768ec6b35080f016b42fa
Felipe <felipe.escorel@gmail.com>
2019-10-17 21:37:30 -0300

    Api printando no log do server :+1:

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { Fragment, Component } from 'react';
 import Header from '../components/Header'
-import ava from '../services/avaApi'
 import styles from '../styles/Default'
 import {
   View,
@@ -15,22 +14,22 @@ import {
 
 
 class Login extends Component {
+  
   constructor(props) {
     super(props)
     this.state = {
       usuario: '',
       senha: '',
       error: '',
+      token: '',
+      userid: '',
     }
   }
-  
+
   /**
    * A API do ava retorna sempre o código 200(sucesso) independende de ter tido sucesso ou não. 
    * Caso consiga uma resposta positiva, retorna um token , caso contrário retorna uma mensagem de erro (Mesmo não sinalizando o erro http)
    */
-  logaAva = token => {
-    response = ava.loginAva(token)
-  }
 
   loginAva = () => {
     var data = new FormData()
@@ -43,17 +42,56 @@ class Login extends Component {
     })
       .then((response) => response.json())
       .then((responseJSON) => {
-        //fazer as outras requisições
-        resposta = responseJSON.token ? this.logaAva(responseJSON.token): this.setState({ error: responseJSON.error })
-
+        // console.log(responseJSON.password) ELE EXIBE A SENHA - criptografar
+        responseJSON.token ? this.setState({ token: responseJSON.token }) : this.setState({ error: responseJSON.error })
       })
+      .then(() => {
+        var data2 = new FormData()
+        data2.append('wstoken', this.state.token)
+        data2.append('wsfunction', 'core_webservice_get_site_info')
+        const response = fetch('http://ava.ufrpe.br/webservice/rest/server.php?moodlewsrestformat=json', {
+          method: 'POST',
+          body: data2
+        })
+          .then((response) => response.json())
+          .then((responseJSON) => {
+            console.log(responseJSON.firstname)
+            console.log(responseJSON.userpictureurl)
+            this.setState({ userid: responseJSON.userid })
+          })
+          .then(() => {
+            var data3 = new FormData()
+            data3.append('wstoken', this.state.token)
+            data3.append('wsfunction', 'core_user_get_users_by_id')
+            data3.append('userids[0]', this.state.userid)
+            const response = fetch('http://ava.ufrpe.br/webservice/rest/server.php?moodlewsrestformat=json', {
+              method: 'POST',
+              body: data3
+            })
+              .then((response) => response.json())
+              .then((responseJSON) => {
+                for (i = 1; i < responseJSON[0].enrolledcourses.length; i++) {
+                  resp = responseJSON[0].enrolledcourses[i].fullname
+                  if (resp.includes('2019.2')) {
+                    console.log(responseJSON[0].enrolledcourses[i].fullname)
+                  }
+                }
+              }).catch((error) => {
+                console.log(error)
+              })
+          })
+          .catch((error) => {
+            //Nunca entra aqui ?
+            console.log(error)
+          })
+      }
+      )
       .catch((error) => {
         //Nunca entra aqui ?
         console.log(error)
       })
   }
-
-
+ 
   render() {
     return (
       <Fragment>
