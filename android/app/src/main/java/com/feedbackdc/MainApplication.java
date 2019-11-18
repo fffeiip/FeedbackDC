1b8b41d587ce4c05a3ebe7ee148c75c105e0fe9c
Felipe <felipe.escorel@gmail.com>
2019-11-17 22:52:07 -0300

    agora sim, api do firebase configurada e com link pelo yarn :+1:

--- android/app/src/main/java/com/feedbackdc/MainApplication.java
+++ android/app/src/main/java/com/feedbackdc/MainApplication.java
@@ -2,7 +2,6 @@ package com.feedbackdc;
 
 import android.app.Application;
 import android.content.Context;
-import io.invertase.firebase.database.ReactNativeFirebaseDatabasePackage;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
-      packages.add(new ReactNativeFirebaseDatabasePackage());
       return packages;
     }
 
